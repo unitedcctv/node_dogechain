fea818d400e750128fe6c7b0d3e48f2d96a2785e
Clark Well <karl@unitedcctv.com>
2014-09-26 14:08:27 +0100

    first commit

---
+++ runner.js
@@ -0,0 +1,7 @@
+var chain = require('chain-node');
+// chain.getAddressBalance('DHSnc4ZHKcLmhZG1AP1cYwTmnDMpdP4GyZ', function (err, resp) {
+//   console.log(resp);
+// });
+// chain.getAddressReceived('DHSnc4ZHKcLmhZG1AP1cYwTmnDMpdP4GyZ', function (err, resp) {
+//   console.log(resp);
+// });
